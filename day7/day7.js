ec55afdcad660cda4f7c77902d5eb3850ac63cb7
Anja Rupnik <anja@bookchoice.com>
2023-01-20 11:17:29 +0100

    day7 part 1

--- day7/day7.js
+++ day7/day7.js
@@ -36,8 +36,71 @@ const setPath = (command) => {
 commands.forEach((command) => {
   if (command.includes("$ cd")) {
     setPath(command);
-    console.log(fileSystem);
-  } else {
-    // sort the files
+  } else if (!command.includes("dir") && !command.includes("$ ls")) {
+    const fileSize = parseInt(command.split(" ")[0]);
+
+    if (!get(currentPath).files) {
+      get(currentPath).files = [];
+    }
+
+    get(currentPath).files.push(fileSize);
   }
 });
+
+const MAX_SIZE = 100000;
+
+const underMaxDirs = [];
+
+const filterOutFiles = (arr) => arr.filter((key) => key !== "files");
+
+const getAllFilesSum = (obj) => {
+  let sum = 0;
+
+  if (obj.files) {
+    sum += obj.files.reduce((a, b) => a + b, 0);
+  }
+
+  const directories = filterOutFiles(Object.keys(obj));
+
+  if (directories.length) {
+    directories.map((dir) => {
+      sum += getAllFilesSum(obj[dir]);
+    });
+  }
+
+  return sum;
+};
+
+const checkFileSize = (obj) => {
+  const mainLevelKeys = filterOutFiles(Object.keys(obj));
+
+  if (!mainLevelKeys.length) {
+    const sum = getAllFilesSum(obj);
+    if (sum < MAX_SIZE) {
+      underMaxDirs.push(sum);
+    }
+    return;
+  }
+
+  mainLevelKeys.map((mainLevelKey) => {
+    const dir = obj[mainLevelKey];
+    const sum = getAllFilesSum(dir);
+
+    if (sum < MAX_SIZE) {
+      underMaxDirs.push(sum);
+    }
+
+    const subDirs = filterOutFiles(Object.keys(dir));
+
+    if (subDirs.length) {
+      subDirs.map((subDir) => {
+        checkFileSize({ [subDir]: dir[subDir] });
+      });
+    }
+  });
+};
+
+checkFileSize(fileSystem);
+
+const resultOne = underMaxDirs.reduce((a, b) => a + b, 0);
+console.log(resultOne);
